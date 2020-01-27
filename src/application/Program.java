292df4fd8e7444af095d9b56b29725a60f1ca11a
Micael Pinheiro <3.1416micael@gmail.com>
2020-01-27 20:53:24 -0300

    findById Implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -9,13 +9,15 @@ import model.entities.Seller;
 
 public class Program {
 	public static void main(String[] args) {
-		Department dp = new Department(1, "books");
-		System.out.println(dp);
-		
-		Seller seller = new Seller(21,"Bob", "bob@gmail.com", new Date(), 3000.00, dp);
 		
 		
-		System.out.println(seller);
+		SellerDao sellerDao = DaoFactory.createSellerDao();
+		
+		Seller seller = sellerDao.findById(3);
+		System.out.println(seller	);
+		
+		
+		
 		
 	}
 }
