ef96c181d6aaf7f72672dc4a5924ef52c2c4259f
Micael Pinheiro <3.1416micael@gmail.com>
2020-01-28 15:48:17 -0300

    insert implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,5 +1,6 @@
 package application;
 
+import java.util.Date;
 import java.util.List;
 
 import model.dao.DaoFactory;
@@ -31,8 +32,10 @@ public class Program {
 			System.out.println(s);
 		}
 		
-		
-		
+		System.out.println("=======TEST 4: seller insert =======");
+		Seller newSeller = new Seller(null, "Greg","greg@gmail.com",new Date(),4000.00, department);
+		sellerDao.insert(newSeller);
+		System.out.println("Inserted! new Id = "+ newSeller.getId());
 		
 	}
 }
